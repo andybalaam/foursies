06b09ce8a0df6b4c504fcfba18ea628d967f75ba
Andy Balaam <andybalaam@artificialworlds.net>
2016-11-08 08:16:23 +0000

    Parsing a board from string representation, and vice versa.

--- src/Board.elm
+++ src/Board.elm
@@ -1,10 +1,38 @@
-module Board exposing (blackPiece, noPiece, parse, pieceAt, whitePiece)
+module Board exposing
+    ( blackPiece
+    , noPiece
+    , parse
+    , pieceAt
+    , strings
+    , toStrings
+    , whitePiece
+    )
+
+
+import String
 
 
 type Piece = NoPiece | WhitePiece | BlackPiece
+type Strings = Strings String String String String
+
+
+type alias Row = ( Piece, Piece, Piece, Piece )
+
 
 type alias Board =
-    {}
+    { pieces :
+        ( Row
+        , Row
+        , Row
+        , Row
+        )
+    }
+
+
+strings : String -> String -> String -> String -> Strings
+strings line1 line2 line3 line4 =
+    Strings line1 line2 line3 line4
+
 
 noPiece : Piece
 noPiece = NoPiece
@@ -23,6 +51,91 @@ pieceAt (x, y) board =
     noPiece
 
 
-parse : String -> String -> String -> String -> Board
-parse line1 line2 line3 line4 =
-    {}
+parse : Strings -> Result String Board
+parse strs =
+    case strs of
+        Strings line0 line1 line2 line3 ->
+            let
+                parsed =
+                    ( parseRow line0
+                    , parseRow line1
+                    , parseRow line2
+                    , parseRow line3
+                    )
+            in
+                case parsed of
+                    (Err e, _, _, _) -> Err (e ++ " (line 1)")
+                    (_, Err e, _, _) -> Err (e ++ " (line 2)")
+                    (_, _, Err e, _) -> Err (e ++ " (line 3)")
+                    (_, _, _, Err e) -> Err (e ++ " (line 4)")
+                    (Ok r0, Ok r1, Ok r2, Ok r3) ->
+                        Ok
+                            { pieces =
+                                ( r0
+                                , r1
+                                , r2
+                                , r3
+                                )
+                            }
+
+
+parseRow : String -> Result String (Piece, Piece, Piece, Piece)
+parseRow line =
+    case (String.toList line) of
+        c0 :: c1 :: c2 :: c3 :: [] ->
+            let ps = (parseChar c0, parseChar c1, parseChar c2, parseChar c3)
+            in
+                case ps of
+                    (Err e, _, _, _) -> Err (e ++ " (char 1)")
+                    (_, Err e, _, _) -> Err (e ++ " (char 2)")
+                    (_, _, Err e, _) -> Err (e ++ " (char 3)")
+                    (_, _, _, Err e) -> Err (e ++ " (char 3)")
+                    (Ok p1, Ok p2, Ok p3, Ok p4) ->
+                        Ok (p1, p2, p3, p4)
+        default ->
+            Err "Line does not contain 4 characters"
+
+
+parseChar : Char -> Result String Piece
+parseChar c =
+    case c of
+        '.' -> Ok noPiece
+        'O' -> Ok whitePiece
+        'X' -> Ok blackPiece
+        default -> Err
+            ( "Invalid character " ++ (toString c)
+                ++ " - expected '.', 'X', or 'O'"
+            )
+
+
+toStrings : Board -> Strings
+toStrings board =
+    let
+        (row0, row1, row2, row3) = board.pieces
+    in
+        Strings
+            (rowToString row0)
+            (rowToString row1)
+            (rowToString row2)
+            (rowToString row3)
+
+
+rowToString : Row -> String
+rowToString row =
+    let
+        (piece0, piece1, piece2, piece3) = row
+    in
+        String.fromList
+            <| pieceToChar piece0
+            :: pieceToChar piece1
+            :: pieceToChar piece2
+            :: pieceToChar piece3
+            :: []
+
+
+pieceToChar : Piece -> Char
+pieceToChar piece =
+    case piece of
+        NoPiece -> '.'
+        WhitePiece -> 'O'
+        BlackPiece -> 'X'
--- tests/BoardTests.elm
+++ tests/BoardTests.elm
@@ -11,18 +11,89 @@ all : Test
 all =
     describe "Tests of the game board parser and formatter"
         [ test "Parsing an empty board" parseEmptyBoard
+        , test "Parsing a board with a long row" parseBoardWithLongRow
+        , test "Parsing a board with a long row" parseBoardWithShortRow
+        , test "Parsing a board with a bad char" parseBoardWithInvalidChar
+        , test "Parsing + formatting a non-empty board" roundTripRandomBoard
         ]
 
 
+parseEmptyBoard : () -> Expect.Expectation
 parseEmptyBoard =
     \() ->
         let
-            board = Board.parse
+            input = Board.strings
                 "...."
                 "...."
+                "...."
+                "...."
+            board = Board.parse input
+        in
+            case board of
+                Err e -> Expect.fail e
+                Ok b ->
+                    allEqual
+                        Board.noPiece
+                        (List.map (\x -> Board.pieceAt (x, 0) b) [0..2])
+
+
+parseBoardWithLongRow : () -> Expect.Expectation
+parseBoardWithLongRow =
+    \() ->
+        let
+            input = Board.strings
+                "...."
+                "..X.."
                 "...."
                 "...."
         in
-            allEqual
-                Board.noPiece
-                (List.map (\x -> Board.pieceAt (x, 0) board) [0..2])
+            Expect.equal
+                (Err "Line does not contain 4 characters (line 2)")
+                (Board.parse input)
+
+
+parseBoardWithShortRow : () -> Expect.Expectation
+parseBoardWithShortRow =
+    \() ->
+        let
+            input = Board.strings
+                "..."
+                "...."
+                "X..."
+                "...."
+        in
+            Expect.equal
+                (Err "Line does not contain 4 characters (line 1)")
+                (Board.parse input)
+
+
+parseBoardWithInvalidChar : () -> Expect.Expectation
+parseBoardWithInvalidChar =
+    \() ->
+        let
+            input = Board.strings
+                "...."
+                "...."
+                "X..."
+                "O.a."
+        in
+            Expect.equal
+                (Err ("Invalid character 'a' - " ++
+                    "expected '.', 'X', or 'O' (char 3) (line 4)"))
+                (Board.parse input)
+
+
+roundTripRandomBoard : () -> Expect.Expectation
+roundTripRandomBoard =
+    \() ->
+        let
+            input = Board.strings
+                "X.O."
+                "...."
+                ".XO."
+                "...."
+            board = Board.parse input
+        in
+            case board of
+                Err e -> Expect.fail e
+                Ok  b -> Expect.equal input (Board.toStrings b)
