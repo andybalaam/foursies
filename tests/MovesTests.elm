c8736d5fe59f1375f8950c0f3c08861d20a2c302
Andy Balaam <andybalaam@artificialworlds.net>
2016-11-08 20:13:53 +0000

    Test for which black pieces can move

--- tests/MovesTests.elm
+++ tests/MovesTests.elm
@@ -12,25 +12,47 @@ import Moves
 all : Test
 all =
     describe "Tests of the move calculator"
-        [ test "Normally all pieces can move" normallyAllPiecesCanMove
+        [ test "Normally all white pieces can move" allWhitePiecesCanMove
+        , test "Normally all black pieces can move" allBlackPiecesCanMove
         ]
 
 
-normallyAllPiecesCanMove : () -> Expect.Expectation
-normallyAllPiecesCanMove =
+allWhitePiecesCanMove : () -> Expect.Expectation
+allWhitePiecesCanMove =
+    forBoard
+        "X..."
+        "...."
+        "...."
+        "O.O."
+        <| \board -> Expect.equal
+            [(0, 3), (2, 3)]
+            ( Moves.whichCanMove Board.whitePiece board )
+
+
+allBlackPiecesCanMove : () -> Expect.Expectation
+allBlackPiecesCanMove =
+    forBoard
+        "...."
+        ".X.."
+        "...."
+        "O.O."
+        <| \board -> Expect.equal
+            [(1, 1)]
+            ( Moves.whichCanMove Board.blackPiece board )
+
+
+-- Helpers
+
+
+forBoard :
+    String -> String -> String -> String ->
+        (Board.Board -> Expect.Expectation) ->
+        (() -> Expect.Expectation)
+forBoard s1 s2 s3 s4 fn =
     \() ->
         let
-            board = Board.parse <| Board.strings
-                "X..."
-                "...."
-                "...."
-                "O.O."
+            board = Board.parse <| Board.strings s1 s2 s3 s4
         in
             case board of
                 Err e -> Expect.fail e
-                Ok b ->
-                    Expect.equal
-                        [ (0, 3)
-                        , (2, 3)
-                        ]
-                        <| Moves.whichCanMove Board.whitePiece b
+                Ok b -> fn b
