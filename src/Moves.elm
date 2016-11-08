2b54fb5112d82caa6a0263bc1d4ee80675f79ff5
Andy Balaam <andybalaam@artificialworlds.net>
2016-11-08 20:44:50 +0000

    Rename piece to side when talking about whose move it is

--- src/Moves.elm
+++ src/Moves.elm
@@ -10,29 +10,29 @@ import Board
 
 
 whichCanMove : Board.Piece -> Board.Board -> List (Int, Int)
-whichCanMove piece board =
+whichCanMove side board =
     let
         (row0, row1, row2, row3) = board.pieces
     in
-        (  whichCanMoveInRow piece 0 row0
-        ++ whichCanMoveInRow piece 1 row1
-        ++ whichCanMoveInRow piece 2 row2
-        ++ whichCanMoveInRow piece 3 row3
+        (  whichCanMoveInRow side 0 row0
+        ++ whichCanMoveInRow side 1 row1
+        ++ whichCanMoveInRow side 2 row2
+        ++ whichCanMoveInRow side 3 row3
         )
 
 whichCanMoveInRow : Board.Piece -> Int -> Board.Row -> List (Int, Int)
-whichCanMoveInRow piece y row =
+whichCanMoveInRow side y row =
     let
         (s0, s1, s2, s3) = row
     in
-        (  whichCanMoveInSquare piece 0 y s0
-        ++ whichCanMoveInSquare piece 1 y s1
-        ++ whichCanMoveInSquare piece 2 y s2
-        ++ whichCanMoveInSquare piece 3 y s3
+        (  whichCanMoveInSquare side 0 y s0
+        ++ whichCanMoveInSquare side 1 y s1
+        ++ whichCanMoveInSquare side 2 y s2
+        ++ whichCanMoveInSquare side 3 y s3
         )
 
 
 whichCanMoveInSquare :
     Board.Piece -> Int -> Int -> Board.Piece -> List (Int, Int)
-whichCanMoveInSquare piece x y thisPiece =
-    if piece == thisPiece then [(x, y)] else []
+whichCanMoveInSquare side x y thisPiece =
+    if side == thisPiece then [(x, y)] else []
